f98ae567a3ec51b0db2a7b17c20fedcc960f313f
Erin Reilly <erinmariereilly@gmail.com>
2019-02-13 16:54:47 -0500

    added hourly means to analysis

--- 02_wq-analysis-2018-averages-sums.R
+++ 02_wq-analysis-2018-averages-sums.R
@@ -20,9 +20,24 @@ saveRDS(wq_daily_means, "wq_daily_means.rds")
 
 ###hourly means for temperature salinity and depth###
 
+wq_full$date<-date(wq_full$datetime)
+wq_full$hour<-hour(wq_full$datetime)
+
+wq_hourly_means<-wq_full %>% 
+  select(date, hour, station, depth_ft, depth_m, salinity, temperature, temp_wl) %>% 
+  group_by(station, date, hour) %>% 
+  summarise_if(is.numeric, mean, na.rm=TRUE)
+
+wq_hourly_means$datetime<-paste0(wq_hourly_means$date, " ", wq_hourly_means$hour, ":00")
+wq_hourly_means$datetime<-ymd_hm(wq_hourly_means$datetime)
+wq_hourly_means$station <- as.factor(wq_hourly_means$station)
+
+saveRDS(wq_hourly_means, "wq_hourly_means.rds")
+
+###Hypoxia instances###
 
 
-View(wq_daily_means)
-a<-ggplot(wq_daily_means)+geom_point(aes(x=date, y=temperature, color=station))
+View(wq_hourly_means)
+a<-ggplot(wq_hourly_means)+geom_point(aes(x=datetime, y=salinity, color=station))
 a
 
