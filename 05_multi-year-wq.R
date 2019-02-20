704ca1e21d82e61a5b9c446c2a6c1e8ac5cfda0a
Erin Reilly <erinmariereilly@gmail.com>
2019-02-20 15:41:46 -0500

    Updated with multiyear water quality things

--- 05_multi-year-wq.R
+++ 05_multi-year-wq.R
@@ -16,16 +16,44 @@ wq<-wq_2010_2017 %>%
   select(datetime=DateTime,station=Station, depth_ft=Depth, temperature=Temp,
          "DO_%" =ODO, DO_mg=ODO_Conc, spcond=SpCond, salinity=Salinity) %>% 
   mutate(depth_m=depth_ft*0.3048) %>% 
-  full_join(wq_2018, .)
+  full_join(wq_2018, .) 
+
+wq$station[wq$station=="North"]<-"north"
+wq$station[wq$station=="South"]<-"south"
+wq$station[wq$station=="Mid"]<-"mid"
 
 wq$year<-as.factor(year(wq$datetime))
-
+wq$date<-date(wq$datetime)
+wq$hour<-hour(wq$datetime)
+  
 saveRDS(wq, "wq_2010_2018.rds")
 
 ##NORTH STATION December 17/18, 2017 depth measurements arbad
 s<-wq %>% 
   filter(depth_ft>=40)
+#####Salnity daily averages#####
+salinity<-wq %>% 
+  select(date, salinity) %>% 
+  group_by(date) %>% 
+  summarize(salinity=mean(salinity))
+saveRDS(salinity, "mean_daily_salinity2010_2018.rds")
+
+salinity2<-wq %>% 
+  select(date, station, salinity) %>% 
+  group_by(date, station) %>% 
+  summarize(salinity=mean(salinity))
+saveRDS(salinity2, "mean_daily_station_salinity2010_2018.rds")
+
+###Hourly DO averages###
+hypoxia<-wq %>% 
+  select(date, hour, station, DO_mg) %>% 
+  group_by(date, hour, station) %>% 
+  summarise(DO_mg=mean(DO_mg)) %>% 
+  filter(DO_mg<=4) %>% 
+  mutate(datetime=ymd_h(paste0(date," ",hour)))
+
+saveRDS(hypoxia, "hourly_hypoxia_2010_2018.rds")
 
 ####test plots for qa/qc###
-a<-ggplot(wq)+geom_point(aes(x=depth_ft, y=depth_m, color=year))         
+a<-ggplot(hypoxia)+geom_point(aes(x=date, y=station, color=station)) +facet_wrap()        
 a
