2c133f5e641e07446f5147d357cabdc9557bcf99
Erin Reilly <erinmariereilly@gmail.com>
2019-03-26 11:56:51 -0400

    oops forgot to save first

--- 10_peet-plot-species-area.R
+++ 10_peet-plot-species-area.R
@@ -17,10 +17,37 @@ peet_2017<-peet_plots_2017 %>%
   group_by(plot, Size) %>% 
   summarise(species_num=mean(Species.Number)) %>%
   rename(plot_size=Size) %>% 
-  mutate(year="2017")
+  mutate(year=as.integer("2017"))
 rm(peet_plots_2017)
 
-species<-peet_2018 %>% 
+species25<-peet_2018 %>% 
   select(Peet_plot, Subplot, `Level(m2)`, ScientificName) %>% 
   group_by(Peet_plot, Subplot, `Level(m2)`) %>%
-  summarise(SpeciesNum=n_distinct(ScientificName))
+  summarise(SpeciesNum=n_distinct(ScientificName)) %>% 
+  filter(`Level(m2)`==0.25) %>% 
+  rename(plot_size=`Level(m2)`)
+species4<-peet_2018 %>% 
+  select(Peet_plot, Subplot, `Level(m2)`, ScientificName) %>% 
+  group_by(Peet_plot, Subplot) %>%
+  summarise(SpeciesNum=n_distinct(ScientificName)) %>% 
+  filter(!Subplot==40) %>% 
+  mutate(plot_size=4)
+species40<-peet_2018 %>% 
+  select(Peet_plot, Subplot, `Level(m2)`, ScientificName) %>% 
+  group_by(Peet_plot) %>%
+  summarise(SpeciesNum=n_distinct(ScientificName)) %>% 
+  mutate(plot_size=40, Subplot=40)
+species<-species25 %>% 
+  bind_rows(., species4, species40)
+rm(species25,species4,species40)
+peet_2018<-species %>% 
+  mutate(plot=paste0("Plot ", Peet_plot)) %>% 
+  group_by(plot, plot_size) %>% 
+  summarise(species_num=mean(SpeciesNum)) %>%
+  mutate(year=as.integer("2018"))
+
+peet_all<-peet_2012_2016 %>% 
+  bind_rows(., peet_2017, peet_2018)
+saveRDS(peet_all, "peet_species_area_2012_2018.rds")
+rm(peet_2012_2016, peet_2017, peet_2018, species)
+
