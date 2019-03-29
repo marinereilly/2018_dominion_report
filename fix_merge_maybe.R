a34c3d749e0ba363979f0880e1a3ae3308747c60
Erin Reilly <erinmariereilly@gmail.com>
2019-03-29 17:42:47 -0400

    I think I might have lost these updated files with the merge because of the downed server.  Restoring them because I think they are right but don't want to delete the old files.

---
+++ fix_merge_maybe.R
@@ -0,0 +1,7 @@
+wy<-readRDS("water_year_2010_2018.rds")
+
+saveRDS(water_year_2010_2018b,"water_year_2010_2018b.rds")
+saveRDS(`windfrequency2010-2018b`, "windfrequency2010-2018b.rds")
+saveRDS(daily_air_temp_2010_2018b,"daily_air_temp_2010_2018b.rds")
+saveRDS(high_winds_2010_2018b,"high_winds_2010_2018b.rds")
+saveRDS(wind_beaufort_scale_2010_2018b, "wind_beaufort_scale_2010_2018b.rds")
